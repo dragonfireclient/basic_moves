32792d1a6f3d5ed8bd9408bd65e47eead2fafa3a
cora <coradelamouche@gmx.ch>
2022-12-05 00:26:16 +0100

    Add client TP transport

--- init.lua
+++ init.lua
@@ -2,6 +2,11 @@ local modname = minetest.get_current_modname()
 local modpath = minetest.get_modpath(modname)
 dofile(modpath .. "/autofly.lua")
 --dofile(modpath .. "/incremental_tp.lua")
-poi.register_transport('TP',function(pos,name)
+
+poi.register_transport('CTP',function(pos,name)
+    minetest.localplayer:set_pos(pos)
+end)
+
+poi.register_transport('STP',function(pos,name)
     minetest.send_chat_message("/teleport "..pos.x..","..pos.y..","..pos.z)
 end)
