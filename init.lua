6131ea7b965d43ef9654d5883a56128a80a5ac3e
cora <coradelamouche@gmx.ch>
2023-08-16 04:53:37 +0200

    Add autosprint

--- init.lua
+++ init.lua
@@ -4,9 +4,18 @@ dofile(modpath .. "/autofly.lua")
 --dofile(modpath .. "/incremental_tp.lua")
 
 poi.register_transport('CTP',function(pos,name)
-    minetest.localplayer:set_pos(pos)
+	minetest.localplayer:set_pos(pos)
 end)
 
 poi.register_transport('STP',function(pos,name)
-    minetest.send_chat_message("/teleport "..pos.x..","..pos.y..","..pos.z)
+	minetest.send_chat_message("/teleport "..pos.x..","..pos.y..","..pos.z)
+end)
+
+local last_sprint
+ws.rg("AutoFsprint","Movement","autoforwardsprint",function()
+	if minetest.settings:get_bool("continuous_forward") then
+		core.set_keypress("special1", true)
+	end
+end,function() end,function()
+		core.set_keypress("special1", false)
 end)
